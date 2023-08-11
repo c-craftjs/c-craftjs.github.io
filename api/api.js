a31535d26b196eb5d8a5d70305f20fbe2125b11d
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2023-08-11 11:44:13 +0700

    new add modul api

---
+++ api/api.js
@@ -0,0 +1,69 @@
+
+// Fungsi untuk melakukan permintaan GET tanpa otentikasi
+export async function CihuyGet(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+// Fungsi untuk melakukan permintaan POST tanpa otentikasi
+export async function CihuyPost(url, body) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    return data;
+  }
+
+// Fungsi untuk melakukan permintaan GET dengan token otentikasi
+export async function CihuyGetWithToken(url, token) {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    const data = await response.json();
+    return data;
+  }
+  
+// Fungsi untuk melakukan permintaan POST dengan token otentikasi
+export async function CihuyPostWithToken(url, body, token) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    return data;
+}
+
+// Fungsi untuk melakukan permintaan GET dengan header "login" dari nilai cookie QrCode WhatsAuth
+export async function CihuyGetWithCookie(url, cookieName) {
+    const cookies = document.cookie.split('; ');
+    let cookieValue = null;
+  
+    for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === cookieName) {
+        cookieValue = value;
+        break;
+      }
+    }
+  
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        login: cookieValue
+      }
+    });
+  
+    const data = await response.json();
+    return data;
+  }
+    
