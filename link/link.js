ab0eafa5d1c1b23184d4ad4ebac4142a58f9b37f
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2023-08-15 14:58:08 +0700

    change link.js

--- link/link.js
+++ link/link.js
@@ -1,9 +1,9 @@
 // root.js
-import {CihuyAppendBody} from "https://c-craftjs.github.io/element/element.js";
+
 export function CihuyRootLink(href, text) {
     const rootLink = document.createElement('a');
     rootLink.href = href;
     rootLink.textContent = text;
-    CihuyAppendBody(rootLink);
+    document.body.appendChild(rootLink);
   }
   
