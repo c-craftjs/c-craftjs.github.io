8642161d115789f309816a658d6432842b9c111b
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2023-08-23 16:08:27 +0700

    hapus in link

--- link/link.js
+++ link/link.js
@@ -1,11 +1,3 @@
-// root.js
-
-export function CihuyRootLink(href, text) {
-    const rootLink = document.createElement('a');
-    rootLink.href = href;
-    rootLink.textContent = text;
-    document.body.appendChild(rootLink);
-  }
 
 
 // 
