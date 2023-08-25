06bb075d6d1085e27aee5a7d13f7791e70e19abc
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2023-08-25 11:35:31 +0700

    add cihuygetcookie decode uit component

--- cookies/cookies.js
+++ cookies/cookies.js
@@ -1,12 +1,14 @@
 
 //fungsi mengambil cookie
 export function CihuyGetCookie(name) {
-  const cookies = document.cookie.split('; ');
+  const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
     const [cookieName, cookieValue] = cookie.split('=');
-    if (cookieName === name) {
-      return cookieValue;
+    if (cookieName.trim() === name) {
+      return decodeURIComponent(cookieValue);
     }
   }
   return null;
 }
+
+
