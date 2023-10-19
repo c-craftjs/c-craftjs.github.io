d50937cb659263b53d6bbe986975864d48d7968e
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2023-10-19 16:59:17 +0700

    barPercentage and categoryPercentage

--- simpelbi/grafik.js
+++ simpelbi/grafik.js
@@ -41,7 +41,7 @@ export function CihuyBarChart(data) {
           elements: {
             bar: {
               borderWidth: 1, // Lebar garis batang
-              barPercentage: 0.1, // Mengatur lebar relatif batang (0.5 adalah setengah lebar default)
+              barPercentage: 0.05, // Mengatur lebar relatif batang (0.5 adalah setengah lebar default)
               categoryPercentage: 0.1, // Mengatur lebar relatif kategori (0.7 adalah 70% lebar default)
             },
           },
