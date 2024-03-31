665a5d4e71dcad928cf1ba1dbae4d197ecd6f037
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2024-03-31 23:19:03 +0700

    content type hapus

--- smk3noku/api.js
+++ smk3noku/api.js
@@ -63,8 +63,7 @@ export function CihuyGetAPI(apiUrl, token, callback) {
   export function CihuyPostApi(url, token, data) {
     const myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`); 
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("access_token", token);
+
 
     const requestOptions = {
         method: 'POST',
